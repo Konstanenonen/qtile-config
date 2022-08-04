ff0e643ac57f6a0a3c7040413a3d783d6859c23d
Konsta Nenonen <konstanenonen@protonmail.com>
2022-08-04 10:47:41 +0300

    My qtile config

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Picom for terminal transparency
+picom -b --experimental-backends --vsync
+
+# Setting the external display to primary
+xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 144.00 --left-of eDP-1
+
+# Wallpaper
+nitrogen --restore
